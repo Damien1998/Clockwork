e92b080a34c4049555422b6be519c0cbf1e7a56b
Ortuy <60518287+Ortuy@users.noreply.github.com>
2020-09-06 20:06:33 +0200

    Etap Przerwy
    lkjkoihuvgycftdxf

---
+++ Assets/Scripts/LevelStart.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelStart : MonoBehaviour
+{
+    public bool playerInRange;
+    public Player[] interactingPlayer;
+    public int levelID = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactingPlayer = new Player[2];
+        interactingPlayer[0] = null;
+        interactingPlayer[1] = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRange && Input.GetButtonDown("Action" + interactingPlayer[0].playerNumber))
+        {
+            SceneManager.LoadScene(levelID);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerInRange = true;
+        if (interactingPlayer[0] == null)
+        {
+            interactingPlayer[0] = collision.GetComponent<Player>();
+            interactingPlayer[0].isByWorkbench = true;
+        }
+        else
+        {
+            interactingPlayer[1] = collision.GetComponent<Player>();
+            interactingPlayer[1].isByWorkbench = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerInRange = false;
+        if (interactingPlayer[0] == collision.GetComponent<Player>())
+        {
+            interactingPlayer[0].isByWorkbench = false;
+            interactingPlayer[0] = null;
+        }
+        else
+        {
+            interactingPlayer[1].isByWorkbench = false;
+            interactingPlayer[1] = null;
+        }
+
+    }
+}
