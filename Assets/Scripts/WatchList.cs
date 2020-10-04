a67f90e709b12a0c0692b2598a5687fb2c916809
Belfaust <Belfaust@users.noreply.github.com>
2020-10-04 17:25:50 +0200

    Checkout Table Init
    Changing up Checkout Tables now they can get lists of watches depending on the level

---
+++ Assets/Scripts/WatchList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "New Watch List",menuName = "watchlist")]
+public class WatchList : ScriptableObject
+{ 
+    public List<GameObject> listOfWatches = new List<GameObject>();
+}
