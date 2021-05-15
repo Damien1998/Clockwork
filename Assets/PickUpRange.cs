036c300adae2cad1f3f6a6889adb7d23642d601c
Ortuy <Ortuy@users.noreply.github.com>
2021-05-15 19:27:54 +0200

    Revert "PickUpRange"
    This reverts commit ccbacab78c99b00546260cdec3309fd0a7c4fc58.

--- Assets/PickUpRange.cs
+++ Assets/PickUpRange.cs
@@ -47,6 +47,7 @@ public class PickUpRange : MonoBehaviour
                 }
                 
                 nearbyItems.OrderBy(item => item.transform.position);
+                nearbyItems.Reverse();
                 itemToPickUpID = nearbyItems.IndexOf(other);
                 
                 nearbyItems[itemToPickUpID].GetComponent<Watch>().isSelected = true;
