b043905db2b1d7e4f9fb576e2874abedfbae44b0
Ortuy <60518287+Ortuy@users.noreply.github.com>
2021-06-22 18:09:59 +0200

    WIP menus (kill me pls)

---
+++ Assets/Scripts/LessAwfulScrollRect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LessAwfulScrollRect : ScrollRect
+{
+
+    override protected void LateUpdate()
+    {
+
+        base.LateUpdate();
+
+        if (this.verticalScrollbar)
+        {
+
+            this.verticalScrollbar.size = 0;
+        }
+    }
+
+    override public void Rebuild(CanvasUpdate executing)
+    {
+
+        base.Rebuild(executing);
+
+        if (this.verticalScrollbar)
+        {
+
+            this.verticalScrollbar.size = 0;
+        }
+    }
+}
