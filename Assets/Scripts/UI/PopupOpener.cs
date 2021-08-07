d951d3ebf7f664b6c421ec89d017a3ac571cbbf1
Ortuy <60518287+Ortuy@users.noreply.github.com>
2021-08-07 20:39:38 +0200

    Circles

---
+++ Assets/Scripts/UI/PopupOpener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupOpener : MonoBehaviour
+{
+    private Button button;
+    [SerializeField]
+    private Popup myPopup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ShowPopup);
+    }
+
+    void ShowPopup()
+    {
+        myPopup.SetPopupPosition(transform.position);
+    }
+}
