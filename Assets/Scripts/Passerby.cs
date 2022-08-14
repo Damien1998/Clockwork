a42f467b4a5d704d0210888d47853a649c6fb00c
Ortuy <60518287+Ortuy@users.noreply.github.com>
2022-08-14 19:47:33 +0200

    Passersby

--- Assets/Scripts/Passerby.cs
+++ Assets/Scripts/Passerby.cs
@@ -5,21 +5,59 @@ using UnityEngine;
 public class Passerby : MonoBehaviour
 {
     private Rigidbody2D rb2D;
+    private Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public ParticleSystem clickFX;
 
     public float direction;
     public float speed;
+
+    private bool isJumpingUp;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        if(direction < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb2D.velocity = new Vector2(direction * speed, 0);
+        if(!isJumpingUp)
+        {
+            rb2D.velocity = new Vector2(direction * speed, 0);
+        }
+        else
+        {
+            rb2D.velocity = Vector2.zero;
+        }
 
+    }
 
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButton(0) && !isJumpingUp)
+        {
+            animator.Play("JumpUp");
+            isJumpingUp = true;
+            direction = -direction;
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+            clickFX.Play();
+        }
+    }
+
+    public void LeaveJumpState()
+    {
+        isJumpingUp = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/PasserbySpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasserbySpawner : MonoBehaviour
+{
+    public float spawnMinInterval, spawnMaxInterval;
+    public float spawnPercent = 100f;
+    public float yRange;
+    public int burstMin, burstMax;
+    public float direction;
+
+    public Sprite[] passerbySprites;
+    public Color[] passerbyColors;
+
+    public float passerbySpeedMin, passerbySpeedMax;
+
+    public bool spawnerActive = true;
+
+    public Passerby passerbyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnPassersby());
+    }
+
+    IEnumerator SpawnPassersby()
+    {
+        while (spawnerActive)
+        {
+            float delay = Random.Range(spawnMinInterval, spawnMaxInterval);
+            
+            var randomRoll = Random.Range(0, 100);
+            if (randomRoll < spawnPercent)
+            {
+                var burst = Random.Range(burstMin, burstMax + 1);
+                for (int i = 0; i < burst; i++)
+                {
+                    float yPos = transform.position.y + Random.Range(-yRange, yRange);
+
+                    Vector2 spawnPos = new Vector2(transform.position.x, yPos);
+
+                    Passerby passerby = Instantiate(passerbyPrefab, spawnPos, Quaternion.identity);
+                    passerby.direction = direction;
+                    passerby.speed = Random.Range(passerbySpeedMin, passerbySpeedMax);
+                    passerby.spriteRenderer.color = passerbyColors[Random.Range(0, passerbyColors.Length)];
+                    passerby.spriteRenderer.sprite = passerbySprites[Random.Range(0, passerbySprites.Length)];
+                }
+            }
+
+            yield return new WaitForSeconds(delay);
+        }
+
+    }
+}
